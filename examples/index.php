bb4933f8ee78a54c786292f316cb9f42c8d9987f
Carlos Buenosvinos <carlos@quepimquepam.com>
2012-07-14 12:44:27 +0200

    Remove bootstrap example

--- examples/index.php
+++ examples/index.php
@@ -1,111 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>CarlosIO Jenkins Dashboard</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Carlos Buenosvinos (carlos@emagister.com)">
-    <meta http-equiv="refresh" content="10">
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
 
-    <!-- Le styles -->
-    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-    <style>
-        body {
-            padding-top: 20px;
-        }
-    </style>
-    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+use CarlosIO\Jenkins\Dashboard;
+use CarlosIO\Jenkins\Source;
+use CarlosIO\Jenkins\Job;
+use CarlosIO\Jenkins\Author;
 
-    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+// Add domain to users (comitters)
+Author::setDomain('<your_domain>');
 
-    <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="bootstrap/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="bootstrap/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="bootstrap/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="bootstrap/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="bootstrap/ico/apple-touch-icon-57-precomposed.png">
-</head>
-
-<body>
-
-<div class="container-fluid">
-    <div class="row-fluid">
-    <?php
-        require_once __DIR__ . '/../vendor/autoload.php';
-
-        use CarlosIO\Jenkins\Dashboard;
-        use CarlosIO\Jenkins\Source;
-        use CarlosIO\Jenkins\Job;
-        use CarlosIO\Jenkins\Author;
-
-        // Add domain to users (comitters)
-        Author::setDomain('emagister.com');
-
-        $dashboard = new Dashboard();
-        // $dashboard->addSource(new Source('http://ci.jenkins-ci.org/view/All/api/json/?depth=2'));
-        $dashboard->addSource(new Source('http://jenkins.php-devel.corp.emagister.com/jenkins/view/All/api/json/?depth=2'));
-        $jobs = $dashboard->getJobs();
-        usort($jobs, "CarlosIO\\Jenkins\\Job::sort");
-
-        $cols = 3;
-        $n = count($jobs);
-
-        $sectors = array();
-        $sectors[] = array_slice($jobs, 0, $n / $cols);
-        $sectors[] = array_slice($jobs, ($n / $cols), $n / $cols);
-        $sectors[] = array_slice($jobs, 2 * ($n / $cols));
-
-        foreach ($sectors as $sector) {
-            ?>
-            <div class="span4">
-                <?php
-                foreach($sector as $job) {
-                    $lastBuild = $job->getLastBuild();
-                    ?>
-                    <div style="padding: 8px 8px 8px 8px" class="alert alert-<?php echo $job->getBootstrapStatus() ?>">
-
-                        <?php if ($job->isInProgress()) { ?>
-                        <div style="margin-bottom: 5px;" class="progress progress-<?php echo $job->getBootstrapProgressBarStatus() ?> progress-striped active">
-                            <div class="bar" style="width: <?php echo $lastBuild->getProgress() ?>%;"></div>
-                        </div>
-                        <?php } ?>
-
-                        <h4 class="alert-heading">
-                            <?php echo $job->getName() ?>
-                        </h4>
-
-                        <?php if (null !== $lastBuild) { ?>
-                        Build Id: <?php echo $lastBuild->getNumber() ?>
-
-                        <?php
-                        $revisions = $lastBuild->getChangeSet()->getRevisions();
-                        if (!empty($revisions)) {
-                            ?>
-                            <?php foreach($lastBuild->getChangeSet()->getRevisions() as $revision) { ?>
-                                Revision: <?php echo $revision->getRevision() ?>
-                                <?php } ?>
-                            <?php } ?>
-                        <?php } ?>
-
-                        <?php if (null !== $lastBuild && count($lastBuild->getAuthors()) > 0) { ?>
-                            <p style="margin: 5px 0 0">
-                            <?php foreach ($lastBuild->getAuthors() as $author) { ?>
-                                <img src="<?php echo $author->getGravatar() ?>" title="<?php echo $author->getName() ?>" alt="<?php echo $author->getName() ?>" width="48" height="48" />
-                            <?php } ?>
-                            </p>
-                        <?php } ?>
-                    </div>
-                    <?php
-                }
-                ?>
-            </div>
-            <?php
-        }
-    ?>
-    </div> <!-- /row -->
-</div> <!-- /container -->
-</body>
-</html>
+$dashboard = new Dashboard();
+$dashboard->addSource(new Source('http://ci.jenkins-ci.org/view/All/api/json/?depth=2'));
+$jobs = $dashboard->getJobs();
+usort($jobs, "CarlosIO\\Jenkins\\Job::sort");
+print_r($jobs);
